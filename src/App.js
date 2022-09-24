c968d483e450805297fbbab7e7ce055a4da691b0
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-09-24 21:13:41 +0600

    small changed

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="heading-logo"></div>
+<div className="search-bar">
+</div>
+<div className="navigation">
+  <div className="navigation-item">
+
+  </div>
+  <div className="navigation-item">
+
+  </div>
+  <div className="navigation-item">
+    
+  </div>
+</div>
     </div>
   );
 }
