80471e06a03f44a43ddc3f6b9f3d2cf59d300da3
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-09-25 19:00:35 +0600

    Navigation item added

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ function App() {
 </div>
 <div className="navigation">
   <div className="navigation-item">
-
+<span className="navigation-itemOne"> Hello </span>
+<span className="navigation-itemTwo">Guest</span>
   </div>
   <div className="navigation-item">
-
+  <span className="navigation-itemOne">Your</span>
+<span className="navigation-itemTwo">Shop</span>
   </div>
   <div className="navigation-item">
-    
+  <span className="navigation-itemOne">Cart</span>
+<span className="navigation-itemTwo">0</span>
   </div>
 </div>
     </div>
