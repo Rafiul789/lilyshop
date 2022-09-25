8935c05d8d8f7b2e3de572c5e0fe57c7ff6bdbcc
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-09-25 20:15:16 +0600

    header component and header css file added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 
 import './App.css';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import StoreIcon from '@material-ui/icons/Store';
+import Header from './Component/Header/Header';
+
 function App() {
   return (
     <div >
-     <div className="heading-logo">
-      <StoreIcon className="header-image"   fontSize="large"/>
-      <h2 className="header-title" >Lilyshop</h2>
-     </div>
-<div className="search-bar">
-</div>
-<div className="navigation">
-  <div className="navigation-item">
-<span className="navigation-itemOne"> Hello </span>
-<span className="navigation-itemTwo">Guest</span>
-  </div>
-  <div className="navigation-item">
-  <span className="navigation-itemOne">Your</span>
-<span className="navigation-itemTwo">Shop</span>
-  </div>
-  <div className="navigation-item">
-  <ShoppingCartIcon fontSize="large" />
-<span className="navigation-itemTwo">0</span>
-  </div>
-</div>
+   <Header></Header>
     </div>
   );
 }
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import StoreIcon from '@material-ui/icons/Store';
+import SearchIcon from '@material-ui/icons/Search';
+import './Header.css';
+const Header = () => {
+    return (
+           <div className="header">
+              <div className="heading-logo">
+      <StoreIcon className="header-image"   fontSize="large"/>
+      <h2 className="header-title" >Lilyshop</h2>
+     </div>
+
+<div className="search-bar">
+  <input type="text" className="search" />
+  <SearchIcon  className="search-icon"/>
+</div>
+
+<div className="navigation">
+  <div className="navigation-item">
+<span className="navigation-itemOne"> Hello </span>
+<span className="navigation-itemTwo">Guest</span>
+  </div>
+  <div className="navigation-item">
+  <span className="navigation-itemOne">Your</span>
+<span className="navigation-itemTwo">Shop</span>
+  </div>
+  <div className="navigation-item">
+  <ShoppingCartIcon className="cart-item"  fontSize="large" />
+<span className="navigation-itemTwo cart-count ">0</span>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Header;
