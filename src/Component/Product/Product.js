935b8c91a99b04ad248b0dab11ad4cfa9f8ce14b
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-09-27 15:31:58 +0600

    product created

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import{faStar} from '@fortawesome/free-solid-svg-icons';
+import product1 from '../../assets/jacket-1.png';
+import './Product.css';
 const Product = () => {
     return (
         <div>
             <div className="product">
                 
+                <div >
+<img className="product-image"   src={product1} alt='product'/>
+
+                </div>
+                <div className="product-info">
+                    <p>Title</p>
+                    <p className="product-price">$50</p>
+                    <p className="product-rating"><FontAwesomeIcon icon={ faStar} /> <FontAwesomeIcon icon={ faStar} /> <FontAwesomeIcon icon={ faStar} /> <FontAwesomeIcon icon={ faStar} /> <FontAwesomeIcon icon={ faStar} />    </p>
+                </div>
+
+   <button>Add to Cart</button>
+
             </div>
         </div>
     );
