1cf13878397609771f47dd7028b2a30b8d4b0875
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-09-28 12:28:16 +0600

    dynamically product added

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,30 +1,64 @@
 import React from 'react';
 import './Home.css';
 import Product from '../Product/Product'
+import product1 from '../../assets/jacket-1.png'
+import product2 from '../../assets/jacket-2.png'
+import product3 from '../../assets/jacket-3.png'
+import product4 from '../../assets/jacket-4.png'
 const Home = () => {
     return (
-        <div>
-            <div className="home   ">
+        
+            <div className="home">
 <div className="home-container">
     <img src='' alt="" className="home-image" />
     <div className="home-row">
-        <Product/>
-        <Product/>
-        <Product/>
+        <Product 
+        id="1"
+        title='Winter Jacket Women'
+        price={11.96}
+        rating={5}
+        image={product1}
+        />
+        <Product
+        id="2"
+        title='Winter Jacket Men'
+        price={15.86}
+        rating={5}
+        image={product2}
+        />
+        <Product id="3"
+        title='Winter Jacket Women'
+        price={9.86}
+        rating={4}
+        image={product3}  />
     </div>
     <div className="home-row">
-        <Product/>
-        <Product/>
+        <Product
+        id="4"
+        title='Winter Jacket Men'
+        price={10.66}
+        rating={4}
+        image={product4}
+        
+        />
+        <Product id="4"
+        title='Winter Jacket Men'
+        price={10.66}
+        rating={4}
+        image={product4} />
     </div>
     <div className="home-row">
     
-        <Product/>
-        <Product/>
+        <Product id="4"
+        title='Winter Jacket Men'
+        price={10.66}
+        rating={4}
+        image={product4}  />
+        
     </div>
 </div>
 
-            </div>
-        </div>
+            </div>     
     );
 };
 
--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import{faStar} from '@fortawesome/free-solid-svg-icons';
-import product1 from '../../assets/jacket-1.png';
+
 import './Product.css';
-const Product = () => {
+const Product = ({id,title,image,rating,price}) => {
     return (
         <div>
             <div className="product">
                 
                 <div >
-<img className="product-image"   src={product1} alt='product'/>
+<img className="product-image"   src={image} alt='product'/>
 
                 </div>
                 <div className="product-info">
-                    <p>Title</p>
-                    <p className="product-price">$50</p>
-                    <p className="product-rating"><FontAwesomeIcon icon={ faStar} /> <FontAwesomeIcon icon={ faStar} /> <FontAwesomeIcon icon={ faStar} /> <FontAwesomeIcon icon={ faStar} /> <FontAwesomeIcon icon={ faStar} />    </p>
+                    <p>{title}</p>
+                    <p className="product-price">${price}</p>
+                    <p className="product-rating"> {rating}{Array(rating).fill().map(()=>( <FontAwesomeIcon icon={ faStar} /> ))}    </p>
                 </div>
 
-   <button>Add to Cart</button>
+   <button className="product-button"  >Add to Cart</button>
 
             </div>
         </div>
