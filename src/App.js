13bcac876af907cfe32100f418838fa9ebb1afa7
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-10-11 11:47:11 +0600

    routing added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@
 import './App.css';
 import Header from './Component/Header/Header';
 import Home from './Component/Home/Home';
-
+import {BrowserRouter as Router,Route,Switch} from  'react-router-dom';
+import Checkout from './Component/Checkout/Checkout';
 function App() {
   return (
     <div >
-   <Header></Header>
-   <Home></Home>
+      <Router>
+<Switch>
+  <Route path='/checkout' >
+<Header></Header>
+<Checkout></Checkout>
+  </Route>
+<Route path='/'>
+<Home></Home>
+</Route>
+
+</Switch>
+
+      </Router>
+  
+   
     </div>
   );
 }
---
+++ src/Component/Checkout/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Checkout.css'
+const Checkout = () => {
+    return (
+        <div>
+            <h1>Hello,This is checkout page</h1>
+        </div>
+    );
+};
+
+export default Checkout;
